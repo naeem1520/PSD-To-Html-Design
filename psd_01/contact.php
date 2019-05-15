bfde85f514c8ab5b0873a7dc63d96737d23c6910
naeem1520 <33579244+naeem1520@users.noreply.github.com>
2019-05-15 14:31:15 +0600

    psd01

---
+++ psd_01/contact.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>psd to html</title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<body>
+    <div class="headersection templete clear">
+    	<div class="logo clear">
+    		<img src="images/logo.jpg" alt="logo"/>
+    		<h2>Our website</h2>
+    		<p>our website description</p>
+    	</div>
+    	<div class="social clear">
+    		<img src="images/fb.jpg" alt="facebook"/>
+    		<img src="images/gmail.jpg" alt="facebook"/>
+    		<img src="images/skype.jpg" alt="facebook"/>
+    		<img src="images/tui.jpg" alt="facebook"/>
+    	</div>
+    </div>
+    <div class="navsection templete">
+    	<ul>
+    		<li><a  href="index.html">HOME</a></li>
+            <li><a  href="about.html">ABOUT</a></li>
+            <li><a id="active" href="Contact.html">CONTANCT</a></li>
+            <li><a href="#">PRIVACY</a></li>
+    	</ul>
+    </div>
+    <div class="slider templete clear">
+    	
+
+    </div>
+    <div class="contentsection templete clear">
+	    <div class="mainsection clear">
+	    	<div class="samepost clear">
+	    		<div class="about clear"><h2>contact us</h2>
+                <p>Your first name: <input type="text" name="first name" placeholder="Enter your first name"></p>
+                <p>Your Last name: <input type="text" name="last name" placeholder="Enter your last name"></p>
+                <p>Your email: <input type="email" name="email" placeholder="Enter your email name"></p>
+                <p>Your password: <input type="password" name="password" placeholder="Enter your password name"></p>
+                <p> <input type="submit" name="submit" value="submit"></p>
+	    		</div>
+	    	</div>
+	    	
+	     	    </div>
+	    <div class="slidersection clear">
+	    	<div class="sameslidersection clear">
+	    	<h2>latest Articles</h2>
+	    	<ul>
+    		<li> <a href="#">This is the first latest articles</a></li>
+    		<li> <a href="#">This is the second latest articles</a></li>
+	    	<li> <a href="#">This is the third articles</a></li>
+	    	<li> <a href="#">This is the fourth latest articles</a></li>
+	    	<li> <a href="#">This is the sixth latest articles</a></li>
+	    </div>
+	    <div class="sameslidersection clear">
+	    	<h2>slidebar</h2>
+	    	<p>some text will be here ome text will be here ome text will be here ome text will be here 
+	    	ome text will be here ome text will be here ome text will be here ome text will be here 
+	    ome text will be here v vome text will be hereome text will be hereome text will be hereome text will be here</p>
+	    </div>
+	    <div class="sameslidersection clear">
+	    	<h2>slidebar</h2>
+	    	<p>some text will be here ome text will be here ome text will be here ome text will be here 
+	    	ome text will be here ome text will be here ome text will be here ome text will be here 
+	    ome text will be here v vome text will be hereome text will be hereome text will be hereome text will be here</p>
+	    </div>
+
+
+    </div>
+    <div class="footer templete clear">
+    	<div class="footermenu clear">
+           <ul>
+    		<li> <a href="#">Home</a></li>
+    		<li> <a href="#">About</a></li>
+    		<li> <a href="#">Contact</a></li>
+    		<li> <a href="#">Privacy</a></li>
+    	</ul>
+    	<div><p>&copy; Yes,Iam stupid</p></div>
+        </div>
+    </div>
+    <div class="fixedicon">
+             <a href="www.facebook.com"><img src="images/fb1.jpg" alt="facebook"/></a>
+             <a href="www.twirter.com"><img src="images/tui1.jpg" alt="twirter"/></a>
+             <a href="www.gmail.com"><img src="images/gmail1.jpg" alt="facebook"/></a>
+             <a href="www.skype.com"><img src="images/skype1.jpg" alt="skype"/></a>
+            
+    </div>
+</body>
+</html>
